2be8a5bf3b00f3102e94f70f2c91e5b1380f9f77
Samuel Uriel Lopez Vaencia <bikuhan@hotmail.com>
2022-12-15 07:48:14 -0600

    sexto commit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,6 +26,6 @@ class LoginController extends Controller
             return back()->with('mensaje', 'Datos Incorrectos');
         }
 
-        return redirect()->route('posts.index', []);
+        return view('posts.index', []);
     }
 }
